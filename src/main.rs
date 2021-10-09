9f53e5c908dadbfdfc8461a4ae09ebbd0ce8c98f
cht <3237126351@qq.com>
2021-10-09 22:13:43 +0800

    wayland screencast test

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+mod portals;
+mod widgets;
+//use portals::RemoteDesktopPage;
+use portals::ScreenCastPage;
+use gtk::prelude::*;
+fn main() {
+    let application = gtk::Application::new(
+        Some("com.gitlab.screencast"),
+        Default::default(),
+    );
+    application.connect_activate(|app|{
+        gst::init().expect("Unable to init gstreamer");
+        let window = gtk::ApplicationWindow::new(app);
+        window.set_title(Some("First GTK Program"));
+        window.set_default_size(350, 70);
+
+
+        let win = ScreenCastPage::new();
+        window.set_child(Some(&win));
+        window.show();
+    });
+    application.run();
+}
---
+++ src/portals/mod.rs
@@ -0,0 +1,6 @@
+mod screencast;
+mod camera;
+mod remote_desktop;
+pub use screencast::ScreenCastPage;
+pub use camera::CameraPage;
+pub use remote_desktop::RemoteDesktopPage;
